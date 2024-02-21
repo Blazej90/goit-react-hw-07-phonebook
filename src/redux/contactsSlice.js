23be164cc13b45b3da49e0c0aa8598049c9b4563
Błażej Bartoszewski <blazejbart@gmail.com>
2024-02-21 14:51:04 +0100

    mockAPI

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,26 +1,56 @@
-import { createSlice, createAction } from '@reduxjs/toolkit';
+// import { createSlice, createAction } from '@reduxjs/toolkit';
 
-const addContact = createAction('contacts/addContact');
-const deleteContact = createAction('contacts/deleteContact');
-const setContacts = createAction('contacts/setContacts');
+// const addContact = createAction('contacts/addContact');
+// const deleteContact = createAction('contacts/deleteContact');
+// const setContacts = createAction('contacts/setContacts');
+
+// const contactsSlice = createSlice({
+//   name: 'contacts',
+//   initialState: [],
+//   reducers: {},
+//   extraReducers: builder => {
+//     builder
+//       .addCase(addContact, (state, action) => {
+//         state.push(action.payload);
+//       })
+//       .addCase(deleteContact, (state, action) => {
+//         return state.filter(contact => contact.id !== action.payload);
+//       })
+//       .addCase(setContacts, (state, action) => {
+//         return action.payload;
+//       });
+//   },
+// });
+
+// export { addContact, deleteContact, setContacts };
+// export default contactsSlice.reducer;
+
+import { createSlice } from '@reduxjs/toolkit';
 
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: [],
-  reducers: {},
+  reducers: {
+    // Dodajemy akcję do ustawiania kontaktów
+    setContacts: (state, action) => {
+      return action.payload; // Ustawiamy kontakty z payloadu akcji
+    },
+    // Dodajemy akcję do dodawania nowego kontaktu
+    addContact: (state, action) => {
+      state.push(action.payload); // Dodajemy nowy kontakt do stanu
+    },
+    // Dodajemy akcję do usuwania kontaktu
+    deleteContact: (state, action) => {
+      return state.filter(contact => contact.id !== action.payload); // Usuwamy kontakt ze stanu na podstawie ID
+    },
+  },
   extraReducers: builder => {
-    builder
-      .addCase(addContact, (state, action) => {
-        state.push(action.payload);
-      })
-      .addCase(deleteContact, (state, action) => {
-        return state.filter(contact => contact.id !== action.payload);
-      })
-      .addCase(setContacts, (state, action) => {
-        return action.payload;
-      });
+    // Tutaj pozostawiamy obsługę asynchronicznych akcji
   },
 });
 
-export { addContact, deleteContact, setContacts };
+// Eksportujemy akcje
+export const { setContacts, addContact, deleteContact } = contactsSlice.actions;
+
+// Eksportujemy reducer
 export default contactsSlice.reducer;
